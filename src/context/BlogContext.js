40b4b5ad385fd61f40e7c8cd4548bf17cbdad58f
julioalcantara <alcantara.juliosousa@gmail.com>
2020-02-18 17:41:56 +0000

    Initial commit

---
+++ src/context/BlogContext.js
@@ -0,0 +1,25 @@
+import createDataContext from './createDataContext';
+
+
+
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogPost':
+            return [...state, { title: `Blog Post #${state.length + 1}`}];
+        default:
+            return state;
+    }
+};
+
+const addBlogPost = dispatch => {
+    return () => {
+        dispatch({ type: 'add_blogPost' });
+    }
+    
+};
+
+export const {Context, Provider } = createDataContext(
+    blogReducer,
+    { addBlogPost },
+    []
+);
